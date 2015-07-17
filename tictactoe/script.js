2ff9d4777d1c73fc0b8f4b249cf1fc391ee97620
David Yoon <david_yoon86@hotmail.com>
2015-07-17 18:14:26 -0400

    clickerswork

--- tictactoe/script.js
+++ tictactoe/script.js
@@ -1,6 +1,6 @@
 
 
-var winningCombo = [];
+var winningCombo = [[0,1,2], [3,4,5], [6,7,8], [2,5,8], [1,4,7], [0,3,6], [0,4,8], [2,4,6]];
 var turnCounter = 0;
 var oArray = [];
 var xArray = [];
@@ -20,18 +20,42 @@ for(var i = 0; i < 9; i++){
 setGame();
 
 
+
+var checkWinner = function(){
+
+	for(var i = 0; i < winningCombo.length; i++){
+		if(oArray.sort().join() === winningCombo[i].join()){
+			console.log("o wins!!!");
+		}else if(xArray.sort().join() === winningCombo[i].join()){
+			console.log("x wins!!!");
+		}
+	}
+}
+
+
+
+
 var startGame = function() {
 	var $spot = $(".box");
 	$spot.on("click", function(event){
 		$(event.target).unbind("click");
+		
 		if(turnCounter % 2 === 0){
-			$(event.target).attr("id","opic");
+			$(event.target).addClass("opic");
+			var $boxNum = parseInt($(event.target).prop("id"));
+			oArray.push($boxNum);
+			console.log(oArray);
 			console.log("O");
-		}	else if(turnCounter % 2 != 0){
-			$(event.target).attr("id","xpic");
+		
+		}else if(turnCounter % 2 != 0){
+			$(event.target).attr("class","xpic");
+			var $boxNum = parseInt($(event.target).prop("id"));
+			xArray.push($boxNum);
 			console.log("X");
 		}
+
 		turnCounter += 1;
+		checkWinner();
 
 	});
 
@@ -39,3 +63,45 @@ var startGame = function() {
 }
 
 startGame();
+
+
+
+
+
+
+// if (winningCombo.indexOf(oArray.sort()) >= 0 )
+
+// var board = [
+// [null,null,null],
+// [null,null,null],
+// [null,null,null],
+// ]
+
+// checkForWin
+// 	rowWin
+// 		if row has three items, all the same
+// 			return win
+// 		else 
+// 			no win
+
+// 		for board[i]
+// 			if board[i][0] === board[i][1] && board[i][1] === board[i][2]
+// 				return win
+
+
+// 	colWin
+
+
+// 	diagWin
+
+// checkForOpportunityToWin
+// 	rowWin
+// 		if row has two items, the same && the third item is null
+// 			placeComputerMove(winningSpace)
+
+// 	colWin
+
+
+//  	diagWin	
+
+
