254d1d2a93a7708a0361dd0dec2161a888f2183a
David Yoon <david_yoon86@hotmail.com>
2015-07-18 18:27:58 -0400

    replay button working for some reason

--- tictactoe/script.js
+++ tictactoe/script.js
@@ -2,8 +2,9 @@
 
 var winningCombo = [[0,1,2], [3,4,5], [6,7,8], [2,5,8], [1,4,7], [0,3,6], [0,4,8], [2,4,6]];
 var turnCounter = 0;
-var oArray = [];
-var xArray = [];
+var oArray = [null, null, null, null, null, null, null, null, null];
+var xArray = [null, null, null, null, null, null, null, null, null];
+var gameOver = false;
 
 
 
@@ -22,56 +23,105 @@ setGame();
 
 
 var checkWinner = function(){
-
-	for(var i = 0; i < winningCombo.length; i++){
-		if(oArray.sort().join() === winningCombo[i].join()){
-			console.log("o wins!!!");
-			var $scoreOne = parseInt($("#score1").text());
-			$scoreOne += 1;
-			console.log($scoreOne);
-			for(var i = 0; i < oArray.length; i++)
+	var score1 = $("#score1").text();
+	if(oArray[0] === "O" && oArray[0] === oArray[1] && oArray[1] === oArray[2]  ||  oArray[3] === "O" && oArray[3] === oArray[4] && oArray[4] === oArray[5]  ||  oArray[6] === "O" && oArray[6] === oArray[7] && oArray[7] === oArray[8]
+		|| oArray[2] === "O" && oArray[2] === oArray[5] && oArray[5] === oArray[8]  ||  oArray[1] === "O" && oArray[1] === oArray[4] && oArray[4] === oArray[7]	||  oArray[0] === "O" && oArray[0] === oArray[3] && oArray[3] === oArray[6]
+		|| oArray[0] === "O" && oArray[0] === oArray[4] && oArray[4] === oArray[8]	||	oArray[2] === "O" && oArray[2] === oArray[4] && oArray[4] === oArray[6]){
+		console.log("O wins");
+		gameOver = true;
 
 
-		}else if(xArray.sort().join() === winningCombo[i].join()){
-			console.log("x wins!!!");
-			var $scoreTwo = parseInt($("#score2").text());
-			$scoreTwo += 1;
-		}
-	}
+
+	} else if(xArray[0] === "X" && xArray[0] === xArray[1] && xArray[1] === xArray[2]  ||  xArray[3] === "X" && xArray[3] === xArray[4] && xArray[4] === xArray[5]  ||  xArray[6] === "X" && xArray[6] === xArray[7] && xArray[7] === xArray[8]
+		|| xArray[2] === "X" && xArray[2] === xArray[5] && xArray[5] === xArray[8]  ||  xArray[1] === "X" && xArray[1] === xArray[4] && xArray[4] === xArray[7]	||  xArray[0] === "X" && xArray[0] === xArray[3] && xArray[3] === xArray[6]
+		|| xArray[0] === "X" && xArray[0] === xArray[4] && xArray[4] === xArray[8]	||	xArray[2] === "X" && xArray[2] === xArray[4] && xArray[4] === xArray[6]){
+		console.log("X wins");
+		gameOver = true;
+	}		
+
+
+
+
+
+	// for(var i = 0; i < winningCombo.length; i++){
+	// 	if(oArray.sort().join() === winningCombo[i].join()){
+	// 		console.log("o wins!!!");
+	// 		var $scoreOne = parseInt($("#score1").text());
+	// 		$scoreOne += 1;
+	// 		console.log($scoreOne);
+			
+
+
+	// 	}else if(xArray.sort().join() === winningCombo[i].join()){
+	// 		console.log("x wins!!!");
+	// 		var $scoreTwo = parseInt($("#score2").text());
+	// 		$scoreTwo += 1;
+	// 	}
+	// }
 }
+
+
 
 
 
 
 var startGame = function() {
+	var $replay = $(".playButton");
 	var $spot = $(".box");
-	$spot.on("click", function(event){
-		$(event.target).unbind("click");
-		
-		if(turnCounter % 2 === 0){
-			$(event.target).addClass("opic");
-			var $boxNum = parseInt($(event.target).prop("id"));
-			oArray.push($boxNum);
-			console.log(oArray);
-			console.log("O");
-		
-		}else if(turnCounter % 2 != 0){
-			$(event.target).attr("class","xpic");
-			var $boxNum = parseInt($(event.target).prop("id"));
-			xArray.push($boxNum);
-			console.log("X");
+	
+
+
+	var proceed = function(event){
+		if(gameOver === false){
+			$(event.target).unbind("click");
+			
+			if(turnCounter % 2 === 0){
+				$(event.target).addClass("opic").addClass("animated").addClass("flip");
+				var $boxNum = parseInt($(event.target).prop("id"));
+				oArray[$boxNum] = "O";
+				console.log(oArray);
+				console.log("O");
+			
+			}else if(turnCounter % 2 != 0){
+				$(event.target).addClass("xpic").addClass("animated").addClass("flip");
+				var $boxNum = parseInt($(event.target).prop("id"));
+				xArray[$boxNum] = "X";
+				console.log(xArray);
+				console.log("X");
+			}
+
+			turnCounter += 1;
+			checkWinner();
+		}else if(gameOver === true){
+			$(".box").unbind("click");
 		}
 
-		turnCounter += 1;
-		checkWinner();
+	}
 
-	});
+	$spot.click(proceed);
+
+
+
+
+
+
+	$replay.on("click", function(){
+		console.log("hello");
+		turnCounter = 0;
+		oArray = [null, null, null, null, null, null, null, null, null];
+		xArray = [null, null, null, null, null, null, null, null, null];
+		gameOver = false;
+		$spot.removeClass("opic").removeClass("xpic").removeClass("animated").removeClass("flip");
+		$spot.click(proceed);
+
+
+	})
+
 
 
 }
 
 startGame();
-
 
 
 
